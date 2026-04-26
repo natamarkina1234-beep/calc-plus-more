a9f371691ce4f60b4e3795842375f6decdac4f2f
natamarkina1234-beep <natamarkina1234@gmail.com>
2026-04-26 20:58:21 +0300

    I added some very new information

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 
-//some new information
+//some very new information
 export default tseslint.config(
   { ignores: ["dist"] },
   {
